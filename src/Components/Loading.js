5bcac7573b3c7fab3c8002fe8b34007f1df7b825
Basirul Billah <basirulbillah@gmail.com>
2022-07-24 02:29:18 +0600

    added calender, create event modal

--- src/Components/Loading.js
+++ src/Components/Loading.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Loading = () => {
     return (
         <div className='h-screen w-screen flex justify-center items-center '>
-            <button className="btn btn-square loading" />
+            <button className="btn btn-primary btn-square loading" />
         </div>
     );
 };
--- src/Pages/Scheduler.js
+++ src/Pages/Scheduler.js
@@ -1,10 +1,116 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { DayPicker } from 'react-day-picker';
+import 'react-day-picker/dist/style.css';
+import { useForm } from "react-hook-form";
 
 const Scheduler = () => {
+    const [date, setDate] = useState(new Date());
+    const { register, handleSubmit, resetField } = useForm();
+    const onSubmit = data => {
+        console.log(data);
+        resetField("eventName");
+        resetField("eventDescription");
+        resetField("date");
+        resetField("time");
+    };
+
     return (
-        <div>
-            <h1 className='text-5xl'>Scheduler</h1>
-        </div>
+        <div className="mx-20">
+            <div className="grid grid-cols-3 gap-5">
+                <div className="col-span-2 flex flex-col mt-7 mb-2">
+                    {/* The button to open modal */}
+                    <label htmlFor="my-modal-6" className="btn btn-primary">Create Event</label>
+
+                    {/* modal */}
+                    <input type="checkbox" id="my-modal-6" className="modal-toggle" />
+                    <div className="modal modal-bottom sm:modal-middle">
+                        <div className="modal-box">
+                            <h3 className="font-bold text-lg">Create Event</h3>
+                            <hr />
+                            <form onSubmit={handleSubmit(onSubmit)}>
+                                {/* event name field */}
+                                <div className="form-control w-full max-w-xs">
+                                    <label className="label">
+                                        <span className="label-text">Event name</span>
+                                    </label>
+                                    <input
+                                        type="text"
+                                        className="input input-bordered input-sm w-full max-w-xs"
+                                        {...register("eventName", {
+                                            required: {
+                                                value: true,
+                                                message: 'Event name is required'
+                                            },
+                                        })
+                                        }
+                                    />
+                                </div>
+
+                                {/* description field */}
+                                <div className="form-control w-full max-w-xs m-full">
+                                    <label className="label">
+                                        <span className="label-text">Description</span>
+                                    </label>
+                                    <textarea
+                                        type="text"
+                                        className="input-bordered textarea w-full max-w-xs"
+                                        {...register("eventDescription")}
+                                    />
+                                </div>
+
+                                {/* event date */}
+                                <div className="flex gap-3">
+                                    <div className="form-control w-full max-w-xs">
+                                        <label className="label">
+                                            <span className="label-text">Date</span>
+                                        </label>
+                                        <input
+                                            type="date"
+                                            className="input input-bordered input-sm w-full max-w-xs"
+                                            {...register("date")}
+                                        />
+                                    </div>
+                                    <div className="form-control w-full max-w-xs">
+                                        <label className="label">
+                                            <span className="label-text">Time</span>
+                                        </label>
+                                        <input
+                                            type="time"
+                                            className="input input-bordered input-sm w-full max-w-xs"
+                                            {...register("time")}
+                                        />
+                                    </div>
+                                </div>
+                                <input className="btn btn-primary mt-3" type="submit" />
+                            </form>
+                        </div>
+                    </div>
+                </div>
+
+                {/* calender */}
+                <div>
+                    <div className="flex">
+                        <div>
+                            <DayPicker
+                                mode='single'
+                                selected={date}
+                                onSelect={setDate}
+                            />
+                        </div>
+                    </div>
+                    <hr />
+                    <div className="p-6">
+                        <h1 className="text-lg font-bold">Events</h1>
+                        <p>02:30 - Mathematical & Probabilistic Analysis</p>
+                        <p>02:30 - Mathematical & Probabilistic Analysis</p>
+                        <p>02:30 - Mathematical & Probabilistic Analysis</p>
+                        <p>02:30 - Mathematical & Probabilistic Analysis</p>
+                        <p>02:30 - Mathematical & Probabilistic Analysis</p>
+                        <p>02:30 - Mathematical & Probabilistic Analysis</p>
+                    </div>
+                </div>
+            </div>
+        </div >
     );
 };
 
